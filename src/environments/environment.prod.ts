4b67f38decf3fe50cb796fb419659756286d7e0c
Radek F <radek.fr@email.cz>
2018-11-01 23:07:55 +0100

    updating environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB1X6w2Vs5eQIBdD7Fzl8RV5KkjFqw5sTA",
+    authDomain: "ng-fitapp.firebaseapp.com",
+    databaseURL: "https://ng-fitapp.firebaseio.com",
+    projectId: "ng-fitapp",
+    storageBucket: "ng-fitapp.appspot.com",
+    messagingSenderId: "1025747530425"
+  }
 };
